dedff791b918421854052878c3eef0dad428efea
Maximiliano Maiz <maximaizrw@gmail.com>
2023-09-17 20:04:22 -0300

    Corrijo autenticacion

---
+++ IntegradorSofftek/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using IntegradorSofftek.DTOs;
+using IntegradorSofftek.Helpers;
+using IntegradorSofftek.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntegradorSofftek.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private TokenJwtHelper _tokenJwtHelper;
+        private readonly IUnitOfWork _unitOfWork;
+        public LoginController(IUnitOfWork unitOfWork, IConfiguration configuration)
+        {
+            _unitOfWork = unitOfWork;
+            _tokenJwtHelper = new TokenJwtHelper(configuration);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(AuthenticateDTO dto)
+        {
+            var userCredentials = await _unitOfWork.UsuarioRepository.AuthenticateCredentials(dto);
+            if (userCredentials is null) return Unauthorized("Dni o clave incorrectas");
+
+            var token = _tokenJwtHelper.GenerateToken(userCredentials);
+
+            var user = new UsuarioLoginDTO()
+            {
+                Nombre = userCredentials.Nombre,
+                Dni = userCredentials.Dni,
+                Token = token
+            };
+
+            return Ok(user);
+        }
+    }
+}
--- IntegradorSofftek/Program.cs
+++ IntegradorSofftek/Program.cs
@@ -1,6 +1,10 @@
 using IntegradorSofftek.DataAccess;
 using IntegradorSofftek.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,14 +13,48 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Autorizacion JWT",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer"
+    });
 
-builder.Services.AddScoped<IUnitOfWork, UnitOfWorkService>();
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
+
+
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer("name=DefaultConnection");
 });
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters()
+{
+    ValidateIssuerSigningKey = true,
+    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+    ValidateIssuer = false,
+    ValidateAudience = false
+});
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWorkService>();
 
 var app = builder.Build();
 
@@ -29,6 +67,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
